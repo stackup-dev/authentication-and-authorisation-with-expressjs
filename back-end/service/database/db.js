2487ae20c265b06a2d0bffd3b41daca23f31da7d
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-08-19 18:35:47 +0800

    sequelize: Sequelize constructor does not need the sqlite invocation
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- back-end/service/database/db.js
+++ back-end/service/database/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("sqlite::memory:", {
+const sequelize = new Sequelize({
 	dialect: "sqlite",
 	storage: "./service/database/db.sqlite",
 	logging: false,
