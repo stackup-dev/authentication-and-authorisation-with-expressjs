c018c6bacadf7f0fdf660d328218ea1211bc569e
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-08-20 19:26:30 +0800

    simplify if-clause and remove unnecessary code
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- back-end/service/security/authorisation.js
+++ back-end/service/security/authorisation.js
@@ -5,7 +5,7 @@ const config = process.env;
 const authorisation = ({ isAdmin }) => {
 	return (req, res, next) => {
 		const user = req.signedCookies;
-		let userHasAdminRole = undefined;
+		let userHasAdminRole = false;
 
 		const xAccessToken = user?.user?.token;
 		console.log("Access Token", xAccessToken);
@@ -13,19 +13,12 @@ const authorisation = ({ isAdmin }) => {
 			const decoded = jwt.verify(xAccessToken, config.TOKEN_KEY);
 			userHasAdminRole = decoded.isAdmin;
 			console.log("Decoded", userHasAdminRole);
-		} else {
-			return res.status(401).send({
-				auth: false,
-				message: "You are not authorised to access this page.",
-				status: 401,
-				payload: null,
-			});
-		}
-		console.log("Access Token", xAccessToken);
-		console.log("Token", user?.user?.token);
+			console.log("Access Token", xAccessToken);
+			console.log("Token", user?.user?.token);
 
-		if (convertToRole(userHasAdminRole) >= convertToRole(isAdmin)) {
-			return next();
+			if (convertToRole(userHasAdminRole) >= convertToRole(isAdmin)) {
+				return next();
+			}
 		}
 		// else was omitted here because of early return
 		console.log("Not authorised.");
